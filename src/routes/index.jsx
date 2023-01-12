5f22cf78782cadeaeb23c84c7842107b330864e7
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2023-01-12 17:24:38 -0300

    feat: Splash page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import UserName from '../pages/UserName';
 import EnterData from '../pages/EnterData';
 import ShowResult from '../pages/ShowResult';
+import Splash from '../components/Splash';
 
 function RoutesManager() {
 
     return(
         <Router>
             <Routes>
-                <Route exact path='/' element={<UserName />} />
+                <Route exact path='/' element={<> <Splash /> <UserName /></>} />
                 <Route path='/EnterData' element={<EnterData />} />
                 <Route path='/ShowResult' element={<ShowResult />} />
             </Routes>
